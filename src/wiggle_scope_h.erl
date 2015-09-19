e629311bbb771ed35d8840f82de940f31d23e80a
Heinz N. Gies <heinz@licenser.net>
2015-09-19 21:46:27 +0200

    Add scopes endpoint, for real

---
+++ src/wiggle_scope_h.erl
@@ -0,0 +1,63 @@
+%% Feel free to use, reuse and abuse the code in this file.
+
+%% @doc Hello world handler.
+-module(wiggle_scope_h).
+-include("wiggle.hrl").
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+-export([allowed_methods/3,
+         permission_required/1,
+         get/1,
+         create/3,
+         read/2,
+         write/3,
+         delete/2]).
+
+-behaviour(wiggle_rest_h).
+
+allowed_methods(_, _Token, []) ->
+    [<<"GET">>].
+
+get(_State)  ->
+    {ok, ls_oauth:scope()}.
+
+permission_required(_State) ->
+    {ok, always}.
+
+%%--------------------------------------------------------------------
+%% GET
+%%--------------------------------------------------------------------
+
+read(Req, State = #state{path = [], obj = Scopes}) ->
+    {to_json(Scopes), Req, State}.
+
+%%--------------------------------------------------------------------
+%% POST
+%%--------------------------------------------------------------------
+
+create(Req, State, _Decoded) ->
+    {fase, Req, State}.
+
+%%--------------------------------------------------------------------
+%% PUT
+%%--------------------------------------------------------------------
+
+write(Req, State, _) ->
+    {false, Req, State}.
+
+%%--------------------------------------------------------------------
+%% DEETE
+%%--------------------------------------------------------------------
+
+delete(Req, State) ->
+    {false, Req, State}.
+
+to_json(Scopes) ->
+    [[{<<"default">>, Default},
+      {<<"description">>, Desc},
+      {<<"scope">>, Scope}]
+     || #{scope := Scope, desc := Desc, default := Default} 
+            <- Scopes].
